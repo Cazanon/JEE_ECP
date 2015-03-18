54c0def123d210928d67ae8e2964981ddd63784b
XTSOPR25 <XTSOPR25@OEPMPC3099.oepm.local>
2015-03-18 10:38:25 +0100

    Update clase: VotoDao

--- src/main/java/persistence/models/daos/VotoDao.java
+++ src/main/java/persistence/models/daos/VotoDao.java
@@ -1,7 +1,12 @@
 package persistence.models.daos;
 
+import java.util.List;
+
+import persistence.models.entities.Tema;
 import persistence.models.entities.Voto;
 
 public interface VotoDao extends GenericDao<Voto, Integer> {
 
+	List<Voto> getVotos(Tema tema);
+	
 }
