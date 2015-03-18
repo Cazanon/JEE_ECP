8a5c11175f8541afa230390dade2700296244dd6
XTSOPR25 <XTSOPR25@OEPMPC3099.oepm.local>
2015-03-18 10:17:15 +0100

    Create Clases DaoJpaFactory y GenericDaoJpa

---
+++ src/main/java/persistence/models/daos/jpa/DaoJpaFactory.java
@@ -0,0 +1,36 @@
+package persistence.models.daos.jpa;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.apache.logging.log4j.LogManager;
+
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.TemaDao;
+import persistence.models.daos.VotoDao;
+
+public class DaoJpaFactory extends DaoFactory {
+    private static final String PERSISTENCE_UNIT = "JEE_ECP";
+
+    private static EntityManagerFactory entityManagerFactory = Persistence
+            .createEntityManagerFactory(PERSISTENCE_UNIT);
+
+    public DaoJpaFactory() {
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        return entityManagerFactory;
+    }
+
+    @Override
+    public TemaDao getTemaDao() {
+        return new TemaDaoJpa();
+    }
+
+    @Override
+    public VotoDao getVotoDao() {
+        return new VotoDaoJpa();
+    }
+
+}
