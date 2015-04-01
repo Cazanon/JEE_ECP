492609d69632599e67b818e69e6ebda7a0c3acb5
Cazanon <carlos.azanon@gmail.com>
2015-04-01 19:17:50 +0200

    Implementado 	VotarController

--- src/main/java/controllers/ejb/VotarController.java
+++ src/main/java/controllers/ejb/VotarController.java
@@ -1,5 +1,29 @@
 package controllers.ejb;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import persistence.models.daos.DaoFactory;
+import persistence.models.entities.Tema;
+import persistence.models.entities.Voto;
+import persistence.models.utils.NivelEstudios;
+
 public class VotarController {
+	
+	public void votar(Voto voto){
+		DaoFactory.getFactory().getVotoDao().create(voto);
+	}
+	
+	public List<Tema> getTemas(){
+		return DaoFactory.getFactory().getTemaDao().findAll();
+	}
+	
+	public List<String> getNivelEstudios(){
+		List<String> niveles = new ArrayList<String>();
+		for(NivelEstudios nivelEstudios : NivelEstudios.values()){
+			niveles.add(nivelEstudios.toString());
+		}		
+		return niveles;
+	}	
 
 }
