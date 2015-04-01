edbee3f20183c1b21e0a4c40bad55eb555ff0a7d
XTSOPR25 <XTSOPR25@OEPMPC3099.oepm.local>
2015-04-01 12:05:24 +0200

    Implementado 	AddTemaController

--- src/main/java/controllers/ejb/AddTemaController.java
+++ src/main/java/controllers/ejb/AddTemaController.java
@@ -1,5 +1,22 @@
 package controllers.ejb;
 
+import persistence.models.daos.DaoFactory;
+import persistence.models.daos.TemaDao;
+import persistence.models.entities.Tema;
+
 public class AddTemaController {
 
+	public void addTema(Tema tema){
+		TemaDao dao = DaoFactory.getFactory().getTemaDao();
+		dao.create(tema);
+	}
+	
+	public boolean existe(Tema tema){
+		TemaDao dao = DaoFactory.getFactory().getTemaDao();
+		if(dao.read(tema.getId())!=null){
+			return true;
+		}
+		return false;
+	}
+	
 }
