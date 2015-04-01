20350d2c954fc6010230922e8647c9c488af8e9a
XTSOPR25 <XTSOPR25@OEPMPC3099.oepm.local>
2015-04-01 11:56:18 +0200

    Update ControllerFactory 	Implementado controllerFactory

--- src/main/java/controllers/ejb/ControllerFactory.java
+++ src/main/java/controllers/ejb/ControllerFactory.java
@@ -1,5 +1,38 @@
 package controllers.ejb;
 
 public class ControllerFactory {
-
+	
+	private AddTemaController addTemaController;
+	private EliminarTemaController eliminarTemaController;
+	private VerVotosController verVotosController;
+	private VotarController votarController;
+	
+	public AddTemaController getAddTemaController(){
+		if(addTemaController==null){
+			addTemaController = new AddTemaController();
+		}
+		return addTemaController;
+	}
+	
+	public EliminarTemaController getEliminarTemaController(){
+		if(eliminarTemaController==null){
+			eliminarTemaController = new EliminarTemaController();
+		}
+		return eliminarTemaController;
+	}
+	
+	public VerVotosController getVerVotosController(){
+		if(verVotosController==null){
+			verVotosController = new VerVotosController();
+		}
+		return verVotosController;
+	}
+	
+	public VotarController getVotarController(){
+		if(votarController==null){
+			votarController = new VotarController();
+		}
+		return votarController;
+	}
+	
 }
