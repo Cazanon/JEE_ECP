57db60a0297a93c000f555e0eb5189f66a29ad68
Cazanon <carlos.azanon@gmail.com>
2015-04-02 19:49:02 +0200

    Create ControllerWs

--- src/main/java/controllers/ControllerFactory.java
+++ src/main/java/controllers/ControllerFactory.java
@@ -1,5 +1,18 @@
 package controllers;
 
-public class ControllerFactory {
+import controllers.ejb.AddTemaEJBController;
+import controllers.ejb.EliminarTemaEJBController;
+import controllers.ejb.VerVotosEJBController;
+import controllers.ejb.VotarEJBController;
+
+public interface ControllerFactory {
+	
+	public AddTemaEJBController getAddTemaController();
+	
+	public EliminarTemaEJBController getEliminarTemaController();
+	
+	public VerVotosEJBController getVerVotosController();
+	
+	public VotarEJBController getVotarController();
 
 }
