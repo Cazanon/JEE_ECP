ee0e1ee40a86f6b2ff5b734137ae6d52512b0090
Cazanon <carlos.azanon@gmail.com>
2015-04-02 11:51:11 +0200

    Update Dispatcher 	Esqueleto inicial

--- src/main/java/views/beans/Dispatcher.java
+++ src/main/java/views/beans/Dispatcher.java
@@ -1,5 +1,46 @@
 package views.beans;
 
-public class Dispatcher {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/jsp/*")
+public class Dispatcher extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	private static String PATH_ROOT_VIEW = "/views/jsp/";
+	
+	@Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+		
+		String action = request.getPathInfo().substring(1);
+
+        String view;
+        switch (action) {
+        case "votar":
+        	view = action;
+        	break;
+        case "verVotos":
+        	view = action;
+        	break;
+        case "addTema":
+        	view = action;
+        	break;
+        case "eliminarTema":
+        	view = action;
+        	break;        	
+        default:
+            view = "home";
+        }
+		
+		this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+        .forward(request, response);		
+	}
 
 }
